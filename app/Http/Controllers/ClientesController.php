410da1e5a9f9a147e0709d79bf297547a2c34e51
Gabish <gabriel.enr1998@gmail.com>
2018-12-22 19:54:52 -0400

    creación del servicio crear y mostrar CLIENTES

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Clientes;
 use Illuminate\Http\Request;
 
 class ClientesController extends Controller
 {
-    //
+    public function  getALL(){
+        $clientes = Clientes::all();
+        return $clientes;
+    }
+
+    public function add(Request $request){
+        $clientes = Clientes::create($request->all());
+        return $clientes;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('clientes','ClientesController@getALL')->name('getAllClientes');
+Route::post('clientes','ClientesController@add')->name('addClientes');
