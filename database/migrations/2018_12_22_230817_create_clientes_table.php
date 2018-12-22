539fbbf200a5daf0ba1971e7d2fd597216100206
Gabish <gabriel.enr1998@gmail.com>
2018-12-22 19:18:06 -0400

    añadiendo bd , controlador y modelo de clientes

---
+++ database/migrations/2018_12_22_230817_create_clientes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Clientes',function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email',40)->unique();
+            $table->string('nombres',100);
+            $table->string('apellidos',100);
+
+            $table->timestamps();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
